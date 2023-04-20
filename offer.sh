ba4e3e6d22158419cc0459d52ebf4fb90f4ac1e7
queenoftech <natinda2006@gmail.com>
2023-04-20 22:58:39 +0000

    rebase practices local

--- offer.sh
+++ offer.sh
@@ -1,3 +1,5 @@
      #!/bin/bash
      echo " I got a 6 figure offer on the 14th of April from Dignari LLC to work for USCIS ,i pray my clearance check goes well so i can get a start date , Amen"
 clearance will go through witgout any hicupps,references will pass too,AMEN
+
+#git rebase practice
