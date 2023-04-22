beb4ffe81e8afb5bb43cfb5e6e2e3a64293a0379
queenoftech <natinda2006@gmail.com>
2023-04-22 18:21:12 +0000

    added some changes

--- git.sh
+++ git.sh
@@ -3,3 +3,6 @@ echo "Hello world"
 #git pull from remote to local area
 #git rebase practices
 #git rebase practice using rebase command
+#git revert command - git reset --hard HEAD~1 
+   THE number is the number of lines of code you want to revert
+#git revert soft ...you can revert back to the last commit and edit it,the command is- git reset HEAD~1
